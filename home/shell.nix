8ea8485c310e7f8bc6c7731a518c75f752c062fd
Leo Newton <leo.newton@icloud.com>
2025-04-15 21:37:56 -0700

    Tweaked to me

---
+++ home/shell.nix
@@ -0,0 +1,17 @@
+{...}: {
+  programs.fish = {
+    enable = true;
+    initExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+  };
+
+  home.shellAliases = {
+    #k = "kubectl";
+    vim = "nvim";
+    ls = "eza -la";
+
+    #urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+    #urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+  };
+}
